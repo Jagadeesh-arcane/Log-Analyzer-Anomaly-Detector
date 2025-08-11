d79c65b44f19729ec9f7ff30e044a12950ec6c1b
Jagadeesh-arcane <jagadeesh.arcane@gmail.com>
2025-08-11 19:16:24 +0530

    Testing pipeline

--- terraform/backend.tf
+++ terraform/backend.tf
@@ -1,6 +1,6 @@
 terraform {
   backend "s3" {
-    bucket         = "terraform-state-bucket"
+    bucket         = "terraform-state-bucket-6546768"
     key            = "log-analyzer-anomaly-detector-public/terraform.tfstate"
     region         = "us-east-1"
     dynamodb_table = "terraform-locks"
