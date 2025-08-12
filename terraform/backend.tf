55681769acf430077a4a509ec8fc2813fc7616e3
Jagadeesh-arcane <jagadeesh.arcane@gmail.com>
2025-08-12 10:21:15 +0530

    Removed all automation deployment codes

--- terraform/backend.tf
+++
@@ -1,9 +0,0 @@
-terraform {
-  backend "s3" {
-    bucket         = "terraform-state-bucket-6546768"
-    key            = "log-analyzer-anomaly-detector-public/terraform.tfstate"
-    region         = "us-east-1"
-    dynamodb_table = "terraform-locks"
-    encrypt        = true
-  }
-}
